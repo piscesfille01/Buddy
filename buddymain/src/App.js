3b6137df3e8422cf4d9c99b1c1618e964b7ee451
piscesfille01 <ellie.kang@mail.utoronto.ca>
2022-02-05 02:24:11 -0500

    updated

--- buddymain/src/App.js
+++ buddymain/src/App.js
@@ -1,18 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import Subscribe from './Subscribe';
+import Input from './Input';
+import Modal from './Modal';
 
 
 function App() {
+
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const openModal = () => {
+    setModalOpen(true);
+  };
+  const closeModal = () => {
+    setModalOpen(false);
+  };
+
   return (
-    <div>
-    <div className='header'>
-      <h1>
-        BUDDY
-      </h1>
-    </div>
-      <Subscribe />
-    </div>
+    <React.Fragment>
+      <div className='header'>
+      <h1>BUDDY!</h1>
+      </div>
+      <div className='buddy'>
+        <h1>GET RECOMMENDATIONS</h1>
+        <h1>FOR YOUR PSYCHOLOGY</h1>
+        <h1>FROM BUDDY!</h1>
+      </div>
+      <button onClick={openModal} style={{float: 'right', marginRight: 75, marginTop: -200,  }} className="subbutton">
+          SUBSCRIBE
+      </button>
+    <Modal open = {modalOpen} close = {closeModal} header = "Enter your email for subscription!">
+    </Modal>
+    </React.Fragment>    
   );
 }
 
---
+++ buddymain/src/Input.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Input = () => {
+    return (
+        <div>
+            <input type="text" placeholder='Enter your email :)' />
+        </div>
+    );
+}
+export default Input;
