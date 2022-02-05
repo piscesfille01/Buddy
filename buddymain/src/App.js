e58f0399960b943b2860c767b80a7d5c06028d5a
piscesfille01 <ellie.kang@mail.utoronto.ca>
2022-02-05 00:27:01 -0500

    init

---
+++ buddymain/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import Subscribe from './Subscribe';
+
+
+function App() {
+  return (
+    <div>
+    <div className='header'>
+      <h1>
+        BUDDY
+      </h1>
+    </div>
+      <Subscribe />
+    </div>
+  );
+}
+
+export default App;
