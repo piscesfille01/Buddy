9180dcd1ebef41353b16cdb84ab16e91e32a2a51
piscesfille01 <ellie.kang@mail.utoronto.ca>
2022-02-05 03:14:50 -0500

    updated

--- buddymain/src/App.js
+++ buddymain/src/App.js
@@ -25,9 +25,17 @@ function App() {
         <h1>FOR YOUR PSYCHOLOGY</h1>
         <h1>FROM BUDDY!</h1>
       </div>
-      <button onClick={openModal} style={{float: 'right', marginRight: 75, marginTop: -200,  }} className="subbutton">
+      <div className='description'>
+        <h1>BUDDY! IS AN EMAIL SUBSCRIPTION PROGRAM THAT SENDS </h1>
+        <h1>PSYCHOLOGICAL RESOURCES TO HELP PEOPLE REGAIN PEACE OF MINDS</h1>
+      </div>
+      <button onClick={openModal} style={{float: 'right', marginRight: 130, marginTop: -180}} className="subbutton">
           SUBSCRIBE
       </button>
+      <button style={{float: 'right', marginRight: 130, marginTop: -90,  }} className='checkbutton'>
+        CHECK WHAT'S NEW!
+      </button>
+      
     <Modal open = {modalOpen} close = {closeModal} header = "Enter your email for subscription!">
     </Modal>
     </React.Fragment>    
