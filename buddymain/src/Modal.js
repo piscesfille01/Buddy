5d7495f5ea763ef521b3e0d341f4ee69bfa9cb66
eun-chae-s <s9710173@naver.com>
2022-02-06 00:37:04 -0500

    fixed the error

--- buddymain/src/Modal.js
+++ buddymain/src/Modal.js
@@ -65,7 +65,7 @@ const Modal = (props) => {
             <button 
               className='submitb'
               id="submit-button"
-              onSubmit={ addNewData }
+              onClick={ addNewData }
             >SUBMIT
             </button>
           </main>
